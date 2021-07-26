7ac9f398a6bb96b1dbd423eee6c324f15d5eeb67
RANK <81941006+AtSignRANK@users.noreply.github.com>
2021-07-26 17:51:11 +0900

    Update Radian.kt

--- src/main/kotlin/io/github/rank/api/radain/Radian.kt
+++ src/main/kotlin/io/github/rank/api/radain/Radian.kt
@@ -1,9 +1,7 @@
 package io.github.rank.api.radain
 
-import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.Contract
 
-@ApiStatus.NonExtendable
 interface Radian {
     var radian: Double
 
