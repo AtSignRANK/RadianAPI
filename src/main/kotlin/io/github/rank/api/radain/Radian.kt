dc6b49c5117f48288e2624a69c12c9d4785004de
RANK <81941006+AtSignRANK@users.noreply.github.com>
2021-07-26 17:45:10 +0900

    Update Radian.kt

--- src/main/kotlin/io/github/rank/api/radain/Radian.kt
+++ src/main/kotlin/io/github/rank/api/radain/Radian.kt
@@ -4,10 +4,8 @@ import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.Contract
 
 @ApiStatus.NonExtendable
-interface Radian(private val angle: Double) {
-    private var radian = angle * 180 / Math.PI
-        get() { return field }
-        set(radian: Double) { field = radian }
+interface Radian {
+    var radian: Double
 
     @Contract(pure = true)
     fun Double.toRadian(): Double { return this / 180 * Math.PI }
