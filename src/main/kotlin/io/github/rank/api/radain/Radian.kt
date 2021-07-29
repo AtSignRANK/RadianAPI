a1f49aa390f3a72f50b664c5fc515b9c71b05a3b
RANK <81941006+AtSignRANK@users.noreply.github.com>
2021-07-29 09:13:30 +0900

    Update Radian.kt

--- src/main/kotlin/io/github/rank/api/radain/Radian.kt
+++ src/main/kotlin/io/github/rank/api/radain/Radian.kt
@@ -1,10 +1,4 @@
 package io.github.rank.api.radain
 
-import org.jetbrains.annotations.Contract
-
-interface Radian {
-    var radian: Double
-
-    @Contract(pure = true)
-    fun Double.toRadian(): Double { return this / 180 * Math.PI }
-}
+@Contract(pure = true)
+public fun Double.toRadian(): Double { return this / 180 * Math.PI }
