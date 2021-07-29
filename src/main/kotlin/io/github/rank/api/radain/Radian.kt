0747c3789739d4affd4ad5a360cf274f08b6602a
RANK <81941006+AtSignRANK@users.noreply.github.com>
2021-07-29 12:46:19 +0900

    Update Radian.kt

--- src/main/kotlin/io/github/rank/api/radain/Radian.kt
+++ src/main/kotlin/io/github/rank/api/radain/Radian.kt
@@ -1,3 +1,5 @@
 package io.github.rank.api.radain
 
-public fun Double.toRadian(): Double { return this / 180 * Math.PI }
+public fun Double.toRadian(): Double { return this * 180 / Math.PI }
+
+public fun Double.fromRadian(): Double { return this / 180  * Math.PI }
